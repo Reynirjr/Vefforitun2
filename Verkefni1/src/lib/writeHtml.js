5cdba237cce2fb4959cd8240bac12f5a4e3e2d5a
Benjamín <brj46@hi.is>
2025-02-05 14:50:32 +0000

    media fix

--- Verkefni1/src/lib/writeHtml.js
+++ Verkefni1/src/lib/writeHtml.js
@@ -28,6 +28,7 @@ export async function writeHtml(indexData) {
       <head>
         <title>Verkefni 1</title>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="./styles.css">
       </head>
       <body>
